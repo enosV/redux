e60611bd17749cd6284c4ba271fa684128faf508
enosvazquez <enosvazquez@gmail.com>
2018-10-02 19:46:33 -0700

    todo

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+import Booklist from '../containers/book-list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+      </div>
+    );
+  }
+}
