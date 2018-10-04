8d3e013946314cddee04a1dd7c227f187721d565
enosvazquez <enosvazquez@gmail.com>
2018-10-03 21:02:51 -0700

    fixed BookList

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 
-import Booklist from '../containers/book-list';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
